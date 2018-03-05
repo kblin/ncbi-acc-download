71b4790b2bf4835085489d4c23c186646281a870
Kai Blin <kblin@biosustain.dtu.dk>
2018-03-05 12:20:58 +0100

    core: Initial tests for download function
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,17 @@
 """Tests for the core module."""
 
-
 from argparse import Namespace
-import requests
+import pytest
 import requests_mock
 
 from ncbi_acc_download import core
+
+
+@pytest.yield_fixture
+def req():
+    """Get requests_mock into the pytest infrastructure."""
+    with requests_mock.mock() as req:
+        yield req
 
 
 def test_config():
@@ -14,4 +20,17 @@ def test_config():
     config = core.Config.from_args(args)
     assert config.verbose is False
     assert config.molecule == 'nucleotide'
+
+
+def test_download_from_ncbi(req, tmpdir):
+    """Test downloading things from NCBI."""
+    req.get(core.NCBI_URL, text='This works.')
+    outdir = tmpdir.mkdir('outdir')
+    filename = outdir.join('foo')
+    expected = outdir.join('foo.gbk')
+    config = core.Config(molecule='nucleotide', verbose=False)
+
+    core.download_from_ncbi('FOO', config, filename=filename)
+
+    assert expected.check()
 
