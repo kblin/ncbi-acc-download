d6a5e4f7dcd8c838ae6e73d7e102efd4e48c8978
Zachary Kurtz <zkurtz@lodotherapeutics.com>
2020-06-04 09:57:40 -0400

    add correct validator to filter out of range proteins

--- ncbi_acc_download/validate.py
+++ ncbi_acc_download/validate.py
@@ -1,16 +1,16 @@
 """Record validation logic."""
 
 import logging
-
 # If Biopython is not available, all checks will return False
 try:
     from Bio import SeqIO
+    from Bio.SeqFeature import BeforePosition, AfterPosition
     HAVE_BIOPYTHON = True
 except ImportError:  # pragma: no cover
     HAVE_BIOPYTHON = False
 
 
-VALIDATION_LEVELS = {'none', 'loads', 'all'}
+VALIDATION_LEVELS = {'none', 'loads', 'all', 'correct'}
 
 
 def run_extended_validation(handle, file_format, validation_level):
@@ -21,12 +21,31 @@ def run_extended_validation(handle, file_format, validation_level):
     handle.seek(0)
     try:
         processed_seq = False
+        records = []
         for rec in SeqIO.parse(handle, file_format):
             processed_seq = True
             if validation_level == 'loads':
                 continue
+            if validation_level == 'correct':
+                ## Correct possible errors from downloaded a restricted-range genbank file
+                ## Will write over the downloaded file
+                newfeats = []
+                for f in rec.features:
+                    if f.type == "CDS" or f.type == "gene":
+                        if not isinstance(f.location.start, BeforePosition) and not isinstance(f.location.end, AfterPosition):
+                            newfeats.append(f)
+                    else:
+                        newfeats.append(f)
+                rec.features = newfeats
+                records.append(rec)
         if not processed_seq:
             logging.error('no seq')
+
+        if validation_level == 'correct':
+            ## rewrite StringIO data
+            handle.truncate(0)
+            handle.seek(0)
+            SeqIO.write(records, handle, 'genbank')
         return processed_seq
     except (ValueError, AssertionError) as err:
         logging.error(err)
