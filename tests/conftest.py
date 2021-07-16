945c2aff98e03693b78d790c32ec209888bceb9c
Kai Blin <kblin@biosustain.dtu.dk>
2021-07-16 11:23:15 +0200

    chore: update pytest test fixture setup
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import requests_mock
 
 
-@pytest.yield_fixture
+@pytest.fixture
 def req():
     """Get requests_mock into the pytest infrastructure."""
     with requests_mock.mock() as req:
